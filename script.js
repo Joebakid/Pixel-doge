e148efc412256b654dbbb6f4666ff38435b47215
joebakid <josephbawo@gmail.com>
2023-12-21 04:08:17 +0100

    adjusted to header

--- script.js
+++ script.js
@@ -23,31 +23,6 @@ navLinks.querySelectorAll(".nav-link").forEach((link) => {
   });
 });
 
-// SCROLL
-let lastScroll = 0;
-
-window.addEventListener("scroll", () => {
-  // console.log(window.pageYOffset);
-  const currentScroll = window.pageYOffset;
-
-  if (currentScroll <= 0) {
-    body.classList.remove("scroll-up");
-  }
-
-  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-    body.classList.remove("scroll-up");
-    body.classList.add("scroll-down");
-  }
-
-  if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
-    body.classList.remove("scroll-down");
-    body.classList.add("scroll-up");
-  }
-
-  // define this in bottom
-  lastScroll = currentScroll;
-});
-
 // GSAP
 // HEADER GALLERY
 // const tlIntro = gsap.timeline({
